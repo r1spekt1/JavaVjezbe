02b9f7416636e3562bd0e36c5be6f5a29dedce84
Pavle Vesovic <119006421+r1spekt1@users.noreply.github.com>
2025-11-09 08:28:15 +0100

    Add files via upload

---
+++ KompleksniBroj/src/KompleksniBroj.java
@@ -0,0 +1,98 @@
+
+public class KompleksniBroj {
+	//Atributi klase
+	
+	private float realniDio;
+	private float imaginarniDio;
+	
+	//konstruktori
+	public KompleksniBroj() { 
+		realniDio = 0;
+		imaginarniDio = 0;
+	}	
+	
+	public KompleksniBroj(float a, float b) {
+		realniDio = a;
+		imaginarniDio = b;
+	}
+	
+	public KompleksniBroj(float a) {
+		realniDio = a;
+		imaginarniDio = 0;	
+	}
+	
+	//geteri i seteri
+	public float dajRealniDio() {
+		return realniDio;
+	}
+	
+	public float dajImaginarniDio() {
+		return imaginarniDio;
+	}
+	
+	public float postaviRealniDio(float a) {
+		realniDio = a;
+	    		return realniDio;
+	}
+	 
+	public float postaviImaginarniDio(float b) {
+		imaginarniDio = b;
+		return imaginarniDio;
+	}
+
+	
+	
+	
+	
+	public void odstampaj() {
+		if (imaginarniDio >= 0) 
+			System.out.printf("%.2f + %.2fi\n", realniDio, imaginarniDio);
+		 else 
+			System.out.printf("%.2f - %.2fi\n", realniDio, -imaginarniDio);
+		
+	}
+
+		
+	//moduo
+		
+	public float moduo() {
+		return (float) Math.sqrt(realniDio * realniDio + imaginarniDio * imaginarniDio);	
+		
+	}
+	
+	//sabiranje
+	
+	public KompleksniBroj saberi(KompleksniBroj a) {
+		KompleksniBroj rez =  new KompleksniBroj();
+		rez.realniDio = realniDio + a.realniDio ; 
+		rez.imaginarniDio = imaginarniDio + a.imaginarniDio;
+		return rez;
+	}
+	//oduzimanje
+	public KompleksniBroj oduzmi(KompleksniBroj a) {
+		KompleksniBroj rez =  new KompleksniBroj();
+		rez.postaviRealniDio(dajRealniDio() - a.dajRealniDio());
+		rez.postaviImaginarniDio(dajImaginarniDio() - a.dajImaginarniDio());
+		return rez;
+	
+	}
+	
+	//mnozenje
+	public KompleksniBroj mnozenje(KompleksniBroj a) {
+		KompleksniBroj rezultat = new KompleksniBroj();
+		rezultat.postaviImaginarniDio(realniDio * a.realniDio - imaginarniDio * a.imaginarniDio);
+		rezultat.postaviImaginarniDio(imaginarniDio * a.realniDio + realniDio * a.imaginarniDio);
+		return rezultat;
+		
+	}
+	
+	//djeljenje 
+	public KompleksniBroj podijeli(kompleksniBroj a) {
+		KompleksniBroj rezultat = new KompleksniBroj();
+		rezultat.postaviRealniDio(dajRealni() * a.dajImaginarni() + dajImaginarni() * a.dajImaginarni() / a.modulBroja() * a.modulBroja()));
+		rezultat.postaviImaginarniDio(dajImaginarni() * a.dajRealni() - dajImaginarni() * a.dajImaginarni() / a.modulBroja() * a.modulBroja()));
+		return  rezultat;
+	
+	}
+	
+}
---
+++ KompleksniBroj/src/KompleksniTest.java
@@ -0,0 +1,31 @@
+
+public class KompleksniTest {
+
+	public static void main(String[] args) {
+		KompleksniBroj z1 = new KompleksniBroj();
+		z1.odstampaj();
+		KompleksniBroj z2 = new KompleksniBroj(-3);
+		z2.odstampaj();
+		KompleksniBroj z3 = new KompleksniBroj(3, -4);
+		z3.odstampaj();
+		z3.postaviRealniDio(-10);
+		z3.odstampaj();
+		System.out.printf("Im{Z3} = %.2f", z3.dajImaginarniDio());
+		System.out.printf("|Z3| = %.2f", z3.moduo());
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
